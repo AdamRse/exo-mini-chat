5f1849dd624206dc687e7b2b7625df0267961bea
AdamRse <bakaarion3@gmail.com>
2024-01-11 16:01:13 +0100

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", "./htmlElements/**/*.html", "./js/*.js"],
+  theme: {
+    extend: {}
+  },
+  safelist: ['ml-30'],
+  plugins: [],
+}
